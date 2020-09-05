b0cc38a6e9e9b1c255d3605ed1c5667eb5bffbae
DEV <dev@grasenhiller.de>
2020-09-05 09:15:04 +0200

    Projektdateien hinzufügen.

---
+++ ESO.Scraper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ESO.Scraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
